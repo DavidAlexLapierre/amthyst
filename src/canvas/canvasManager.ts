f8fbbaf0956fa58b410a1f1e473b0a2ea1c74aad
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2023-08-15 16:12:43 -0400

    merp

--- src/canvas/canvasManager.ts
+++ src/canvas/canvasManager.ts
@@ -2,6 +2,7 @@ export class CanvasManager {
     public initCanvas(width: number, height: number, rootId?: string) {
         const root = rootId ? document.getElementById(rootId) : document.body;
         const canvas = document.createElement('canvas');
+        canvas.id = "amethyst-canvas";
         canvas.width = width;
         canvas.height = height;
         canvas.style.background = 'rgba(20, 20, 20, 1.0)';
