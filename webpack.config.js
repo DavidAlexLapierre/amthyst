79cc1477cb309568c0dde3929146a2428d101d8a
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2023-08-15 13:58:41 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.ts', // Entry point of your application
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'out'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'], // Allow importing TypeScript and JavaScript files
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], // For handling CSS files
+            },
+        ],
+    },
+};
