88c3908c6efcb4497fcc90e3d785f6583346d893
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2023-08-15 14:17:20 -0400

    ignore test files when building

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = (env) => {
                     test: /\.css$/,
                     use: ['style-loader', 'css-loader'], // For handling CSS files
                 },
+                {
+                    test: /\.spec\.ts$/,
+                    use: 'ignore-loader'
+                }
             ],
         },
     }
